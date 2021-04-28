da3224bf8781f85433cf37b190166b593ee0d9b1
henry <henry.zhang@hey.com>
2021-04-28 14:12:56 -0700

    observer pattern (pub sub)

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
+<!-- @format -->
+
 ## Design Patterns in TypeScript
 
-A demo of Design Patterns in TS with an in-memory DB.
+A demo of Design Patterns in nodejs TS with an in-memory DB.
+
+
+- Singleton
+- Factory
+- Observer
 
 ```
 ts-node index.ts
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,33 @@
+type Listener<EventType> = (ev: EventType) => void;
 
 // Observer - PubSub
+function createObs<EventType>(): {
+  subscribe: (listener: Listener<EventType>) => () => void;
+  publish: (event: EventType) => void;
+} {
+  let listeners: Listener<EventType>[] = [];
 
+  return {
+    subscribe: (listener: Listener<EventType>): (() => void) => {
+      listeners.push(listener);
+      return () => {
+        listeners = listeners.filter(l => l !== listener);
+      };
+    },
+    publish: (ev: EventType) => {
+      listeners.forEach(l => l(ev));
+    },
+  };
+}
+
+interface BeforeSetEvent<T> {
+  value: T;
+  newValue: T;
+}
+
+interface AfterSetEvent<T> {
+  value: T;
+}
 
 interface Pokemon {
   id: string;
@@ -15,6 +42,9 @@ interface BaseRecord {
 interface Database<T extends BaseRecord> {
   set(newValue: T): void;
   get(id: string): T | undefined;
+
+  onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void;
+  onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void;
 }
 
 // Factory
@@ -23,30 +53,61 @@ function createDb<T extends BaseRecord>() {
     private db: Record<string, T> = {};
 
     // called on class itself, cant be called on instances of the class
-    static instance:InMemoryDb = new InMemoryDb();
+    static instance: InMemoryDb = new InMemoryDb();
+
+    private beforeAddListeners = createObs<BeforeSetEvent<T>>();
+    private afterAddListeners = createObs<AfterSetEvent<T>>();
 
     public set(newValue: T): void {
+      this.beforeAddListeners.publish({
+        newValue,
+        value: this.db[newValue.id],
+      });
       this.db[newValue.id] = newValue;
+
+      this.afterAddListeners.publish({
+        value: newValue,
+      });
     }
 
     public get(id: string): T | undefined {
       return this.db[id];
     }
+
+    onBeforeAdd(listener: Listener<BeforeSetEvent<T>>): () => void {
+      return this.beforeAddListeners.subscribe(listener);
+    }
+    onAfterAdd(listener: Listener<AfterSetEvent<T>>): () => void {
+      return this.afterAddListeners.subscribe(listener);
+    }
   }
-    // // Singleton method 1
-    // const db = new InMemoryDb();
-    // return db;
+  // // Singleton method 1
+  // const db = new InMemoryDb();
+  // return db;
 
   // Singleton method 2 with static method
   return InMemoryDb;
 }
 
-const pokemonDB = createDb<Pokemon>();
+const PokemonDB = createDb<Pokemon>();
+const unsub = PokemonDB.instance.onAfterAdd(({
+  value
+}) => {
+  console.log(value);
+})
 
-pokemonDB.instance.set({
-  id: "Pikachu",
+PokemonDB.instance.set({
+  id: 'Pikachu',
   attack: 50,
   defense: 10,
 });
 
-console.log(pokemonDB.instance.get("Pikachu"));
+// unsub();
+
+PokemonDB.instance.set({
+  id: 'Pichu',
+  attack: 20,
+  defense: 5,
+});
+
+unsub();
